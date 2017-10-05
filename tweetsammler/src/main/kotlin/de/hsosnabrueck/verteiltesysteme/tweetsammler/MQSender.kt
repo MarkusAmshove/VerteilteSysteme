bef4314ee21bb59c19ca59d0339723ad0c221e6b
Markus Amshove <scm@amshove.org>
2017-10-05 09:15:05 +0200

    Tweetsammler queue

--- tweetsammler/src/main/kotlin/de/hsosnabrueck/verteiltesysteme/tweetsammler/MQSender.kt
+++ tweetsammler/src/main/kotlin/de/hsosnabrueck/verteiltesysteme/tweetsammler/MQSender.kt
@@ -1,8 +1,7 @@
 package de.hsosnabrueck.verteiltesysteme.tweetsammler
 
 import com.google.gson.Gson
-import com.rabbitmq.client.Channel
-import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.*
 import twitter4j.GeoLocation
 
 class MQSender(private val host: String, private val queueName: String) {
@@ -23,4 +22,39 @@ class MQSender(private val host: String, private val queueName: String) {
         val message = Gson().toJson(geoLocation)
         channel.basicPublish("", queueName, null, message.toByteArray())
     }
+}
+
+class MQListener(private val host: String, private val queueName: String) {
+    private var channel: Channel
+    init {
+        val connectionFactory = ConnectionFactory()
+        connectionFactory.host = host
+        connectionFactory.username = "gast"
+        connectionFactory.password = "gast"
+        val newConnection = connectionFactory.newConnection()
+        channel = newConnection.createChannel()
+        channel.queueDeclare(queueName, true, false, false, mutableMapOf())
+        channel.basicConsume(queueName, true, NullConsumer())
+    }
+}
+
+private class NullConsumer : Consumer {
+    override fun handleRecoverOk(consumerTag: String?) {
+    }
+
+    override fun handleConsumeOk(consumerTag: String?) {
+    }
+
+    override fun handleShutdownSignal(consumerTag: String?, sig: ShutdownSignalException?) {
+    }
+
+    override fun handleCancel(consumerTag: String?) {
+    }
+
+    override fun handleDelivery(consumerTag: String?, envelope: Envelope?, properties: AMQP.BasicProperties?, body: ByteArray?) {
+    }
+
+    override fun handleCancelOk(consumerTag: String?) {
+    }
+
 }
