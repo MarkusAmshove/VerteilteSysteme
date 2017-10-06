53a2f845d2c2e95f52e26c6d2086368316bec1df
Markus Amshove <markus@Markus-Laptop.local>
2017-10-06 08:39:29 +0200

    Healtchecker

--- healthchecker/healthchecker.py
+++ healthchecker/healthchecker.py
@@ -6,7 +6,6 @@ import pika
 import json
 from pika import PlainCredentials
 
-PROGRAMM_QUEUE = "starts"
 
 def ermittle_consumer_pro_queue(command):
     proc = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE)
@@ -24,24 +23,26 @@ def ermittle_programm_name(consumername):
         'gefundenesWetter': 'Wettersammler',
         'rawGeoLocations': 'Wetterfinder',
         'wetterStatistik': 'Auswertungsprogramm',
-        'tweetSammler': 'Tweetsammler'
+        'tweetSammler': 'Tweetsammler',
+        'starts': 'Startschlange'
     }[consumername]
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters('192.168.0.151', credentials=PlainCredentials("gast", "gast")))
-
-programm_channel = connection.channel()
-programm_channel.queue_declare(PROGRAMM_QUEUE, durable=True)
 
 
 if __name__ == '__main__':
+    PROGRAMM_QUEUE = "starts"
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters('192.168.1.1', credentials=PlainCredentials("gast", "gast")))
+
+    programm_channel = connection.channel()
+    programm_channel.queue_declare(PROGRAMM_QUEUE, durable=True)
     while True:
-        time.sleep(2)
+        time.sleep(9)
         konsumenten_queues = ermittle_consumer_pro_queue('curl -i -u guest:guest http://localhost:15672/api/queues/')
         cls()
         for r in konsumenten_queues:
             programmname= ermittle_programm_name(str(r['name']))
             print("Programm: " + programmname + " Anzahl: " + str(r['consumers']))
-            if r['consumers'] < 1:
-                print("\t Starte neuen " + programmname
+            if r['name'] !=  'starts' and r['consumers'] < 1:
+                print("\t Starte neuen " + programmname)
                 programm_channel.basic_publish(exchange='',routing_key=PROGRAMM_QUEUE,body=programmname)
